d569833b166279495f5e7621e4af9e8099eeb4dc
Keith Chu <cqroot@outlook.com>
2026-05-02 02:30:21 +0800

    refactor: display hosts in tree format with lipgloss styling

--- cmd/host.go
+++ cmd/host.go
@@ -18,14 +18,22 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/cqroot/minop/pkg/executor"
-	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 )
 
-// RunHostCmd displays all configured hosts in a table.
+var treeStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("212"))
+
+var hostStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("255"))
+
+var groupStyle = lipgloss.NewStyle().
+	Bold(true).Faint(false).Foreground(lipgloss.Color("12"))
+
 func RunHostCmd(cmd *cobra.Command, args []string) {
 	e := executor.New(
 		executor.WithVerboseLevel(flagVerboseLevel),
@@ -34,25 +42,30 @@ func RunHostCmd(cmd *cobra.Command, args []string) {
 	hostGroup, _, err := e.LoadConfig(flagConfigFile)
 	CheckErr(err)
 
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.SetStyle(table.StyleRounded)
-	t.AppendHeader(table.Row{"Group", "Address", "User", "Port"})
-
-	isFirst := true
-	for group, hosts := range hostGroup {
-		for _, host := range hosts {
-			if isFirst {
-				t.AppendRow(table.Row{group, host.Address, host.User, host.Port})
-				isFirst = false
-			} else {
-				t.AppendRow(table.Row{"", host.Address, host.User, host.Port})
-			}
-		}
-		t.AppendSeparator()
-		isFirst = true
+	groups := make([]string, 0, len(hostGroup))
+	for group := range hostGroup {
+		groups = append(groups, group)
 	}
-	t.Render()
+
+	fmt.Fprintln(os.Stdout)
+	for idx, group := range groups {
+		hosts := hostGroup[group]
+		fmt.Fprintf(os.Stdout, "  %s\n", groupStyle.Render("• "+group))
+		for hostIdx, host := range hosts {
+			var branch string
+			if hostIdx == len(hosts)-1 {
+				branch = treeStyle.Render("  └──")
+			} else {
+				branch = treeStyle.Render("  ├──")
+			}
+			fmt.Fprintf(os.Stdout, "%s %s\n",
+				branch,
+				hostStyle.Render(fmt.Sprintf("%s@%s:%d", host.User, host.Address, host.Port)))
+		}
+		if idx < len(groups)-1 {
+			fmt.Fprintln(os.Stdout)
+		}
+	}
 }
 
 // NewHostCmd creates the host command that lists all configured hosts.
